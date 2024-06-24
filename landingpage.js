bf971f163cccbe7b2f916212fd3c4abbecc9c455
Tasmiya Fatima <168153875+fatimaatasmiya@users.noreply.github.com>
2024-06-23 22:43:33 -0700

    Add files via upload
    Task 1

---
+++ landingpage.js
@@ -0,0 +1,19 @@
+
+ScrollReveal({ 
+    reset: true,
+    distance:'60px',
+    duration:2500,
+    delay:100
+
+ });
+ ScrollReveal().reveal('.nav_container', { origin:'right' });
+ ScrollReveal().reveal('.header_container', { origin:'left' });
+ ScrollReveal().reveal('.header_form', { origin:'left' });
+ ScrollReveal().reveal('.about_container', { origin:'right' });
+ ScrollReveal().reveal('.department_card' ,{ interval:100 });
+ ScrollReveal().reveal('.doctors_card' ,{ interval:100 });
+
+
+
+
+ 
